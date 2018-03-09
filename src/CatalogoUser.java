6190ad152f0f3f130982b99bea5a622394cc63e1
bruceandroid <brunocea.1@gmail.com>
2018-03-09 14:48:33 +0000

    CatUser  e User - Diogo

---
+++ src/CatalogoUser.java
@@ -0,0 +1,60 @@
+package domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Classe que define catalogos de user
+ * @author minunes
+ */
+public class CatalogoUser {
+	
+	private static CatalogoUser instance;
+	private Map<String,User> users;
+	
+	/**
+	 * Construtor
+	 */
+	private CatalogoTiposSensor() { 
+		users = new HashMap<>();
+	}
+	
+	public static CatalogoUser getCatalogo() {
+		if (instance == null){
+			instance = new CatalogoUser();
+		}
+		return instance;
+	}
+
+	/**
+	 * Adicionar um novo tipo de sensor a este catalogo
+	 * @param tp - O tipo de sensor a adicionar
+	 * @requires tp != null
+	 */
+	public boolean addUser(String userID) {
+		
+		if(containsUser(userID))
+		    return false;
+		else {
+		    users.put(userID, new User (userID));
+		    return true;
+	    }
+	}
+	
+	public boolean containsUser( String userID) {
+		return users.constainsKey(userID);
+	}
+	
+	public User getUser(String userID) {
+		return users.get(userID);
+	}
+
+	/**
+	 * Os nomes dos tipos de sensores existentes neste catalogo
+	 * @return Uma colecao iteravel com os nomes dos tipos de sensores        
+	 */
+	public Iterable<String> getUsers() {
+		return users.keySet();
+	}
+
+}
