1c17a06ad710659cde5741a89a9f704600209c45
dfogox <diogocariaferreira@gmail.com>
2018-03-19 18:44:20 +0000

    Session Handler e respetivas alterações necessarias nomeadamente, criação de nova excepção

---
+++ src/exceptions/WrongUserPassowrdException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class WrongUserPassowrdException extends Exception {
+}
---
+++ src/handlers/SessionHandler.java
@@ -0,0 +1,29 @@
+package handlers;
+
+import catalogs.CatalogoUser;
+import domain.*;
+import exceptions.WrongUserPassowrdException;
+
+public class SessionHandler extends GodHandler {
+
+    private static CatalogoUser catUser;
+
+    public SessionHandler() {
+        this.curr = curr;
+        catUser = CatalogoUser.getCatalogo();
+    }
+
+    public boolean startSession(String userID, String pass) throws WrongUserPassowrdException{
+
+        User u = catUser.getUser(userID);
+        if (u == null) {
+            catUser.addUser(new User(userID, pass));
+            return false;
+        }
+        if (u.getPass().equals(pass))
+            return true;
+
+        throw new WrongUserPassowrdException();
+
+    }
+}
