ff8f9f42dd07fc9ce94e0999d429e117d2b0ad1f
DfogoX <diogocariaferreira@gmail.com>
2018-04-19 20:51:48 +0100

    fase inicial, quase ok

--- src/managers/manUsers.java
+++ src/managers/manUsers.java
@@ -10,6 +10,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,41 +18,69 @@ public class manUsers {
 
 
     private static File fl = new File("Files");
-    private static File pass = new File(fl, "users.txt");
-    private static File mac = new File(fl, "sec.mac");
+    private static File passFile = new File(fl, "users.txt");
+    private static File macFile = new File(fl, "sec.mac");
 
 
     public static void main (String[] args) {
 
     try {
 
-        fl.mkdir();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Administrador, qual a password?");
-        String password = scanner.next();
 
         final Random r = new SecureRandom();
         byte[] salt = new byte[64];
         r.nextBytes(salt);
 
+        //Cração da pasta
+        fl.mkdir();
+
+
+        //autenticação do admin
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Administrador, qual a password?");
+        String password = scanner.next();
+        //TODO: ver se a pass está bem
+
+        //codificação de pass do admin
         PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 20);
         SecretKeyFactory kf = SecretKeyFactory.getInstance("PBEWithHmacSHA256AndAES_128");
         SecretKey key = kf.generateSecret(keySpec);
 
-        fl.mkdir();
-        FileWriter fw = new FileWriter(new File(fl, "users.txt"), true);
-        FileReader fr = new FileReader(new File(fl, "users.txt"));
+
+        //se não é a primeira execução
+        fl.isDirectory();
+        if(fl.list().length > 0) {
+
+            FileInputStream fPass = new FileInputStream(passFile);
+            FileInputStream fMac = new FileInputStream(macFile);
+
+            //verificar se ficheiro das pass's n foi alterado
+            if(!Arrays.equals(obtainMac(key), Files.readAllBytes(macFile.toPath())))
+                //TODO: para execução
+            {
+
+            }
 
 
 
+
+        }
+
+/*
+
+        //codificação de pass, n sei se é para já
+        PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 20);
+        SecretKeyFactory kf = SecretKeyFactory.getInstance("PBEWithHmacSHA256AndAES_128");
+        SecretKey key = kf.generateSecret(keySpec);
+
+*/
 
     }
     catch (NoSuchAlgorithmException e) {
         e.printStackTrace();
     } catch (InvalidKeySpecException e) {
         e.printStackTrace();
-    } catch (FileNotFoundException e) {
+    } catch (InvalidKeyException e) {
         e.printStackTrace();
     } catch (IOException e) {
         e.printStackTrace();
@@ -66,7 +95,16 @@ public class manUsers {
 
         Mac mac = Mac.getInstance("HmacSHA256");
         mac.init(k);
-        byte[] bt = Files.readAllBytes(pass.toPath());
+        byte[] bt = Files.readAllBytes(passFile.toPath());
         return mac.doFinal(bt);
     }
 }
+
+/*
+
+duvidas:
+como defenir qual a pass do admin
+como verificar se o ficheiro das passes esta ok
+
+
+*/
